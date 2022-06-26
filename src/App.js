6f560f97b3c702e0aca0ed7e1ffde57e14b6e0b3
Antonio Arauujo Pavao <antonioppavao@gmail.com>
2022-06-26 20:10:44 -0300

    Creating css and calling api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
-import React from "react";
+import { useEffect } from "react";
+
+const API_URL = "http://www.omdbapi.com?apikey=2c5117a7";
 
 const App = () => {
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    console.log(data.Search[0]);
+  };
+  useEffect(() => {
+    searchMovies("Spiderman");
+  }, []);
   return <h1>App</h1>;
 };
 
